a6d2ffe911bf7303a32cf4ead6ae36bfc33b9ce3
William Almy <walmy@ucsc.edu>
2024-06-06 10:03:18 -0500

    demo cp2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 # Load the data
 df = pd.read_csv("data/trade-register.csv", skiprows=11)
@@ -7,15 +7,15 @@ df = pd.read_csv("data/trade-register.csv", skiprows=11)
 # Since the dataframe has a MultiIndex and only one column, we need to reset the index and rename the columns appropriately
 df.reset_index(inplace=True)
 df.columns = df.columns.map(lambda x: x.strip())
-df['YO'] = pd.to_datetime(df['Year of order'], format='%Y')
+df["YO"] = pd.to_datetime(df["Year of order"], format="%Y")
 
 st.write("Fields:")
 st.text(str(df.columns))
 print(df.info())
 
 st.write("Filter by date range:")
-start_date = st.date_input("Start date", df['YO'].min().date())
-end_date = st.date_input("End date", df['YO'].max().date())
+start_date = st.date_input("Start date", df["YO"].min().date())
+end_date = st.date_input("End date", df["YO"].max().date())
 
 # Display the dataframe
 st.write("Dataframe:")
@@ -23,14 +23,14 @@ st.dataframe(df)
 
 # Example plot: Number of Orders by Year
 st.write("Number of Orders by Year:")
-if 'Year of order' in df.columns:
-    orders_by_year = df.groupby('Year of order').size()
+if "Year of order" in df.columns:
+    orders_by_year = df.groupby("Year of order").size()
     st.line_chart(orders_by_year)
 
 st.write("Number of Orders by Supplier:")
-orders_by_year = df.groupby('Supplier').size()
+orders_by_year = df.groupby("Supplier").size()
 st.line_chart(orders_by_year)
 
 st.write("Number of Orders by Weapon designation:")
-orders_by_year = df.groupby('Weapon designation').size()
+orders_by_year = df.groupby("Weapon designation").size()
 st.line_chart(orders_by_year)
--- scratch.py
+++ scratch.py
@@ -1,169 +1,303 @@
 import math
+import tempfile
+from io import StringIO
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import streamlit as st
-import streamlit.components.v1 as components
+import geopandas as gpd
 import networkx as nx
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit as st
+import streamlit.components.v1 as components
 from pyvis.network import Network
 
-
 APAC_COUNTRIES = [
-    "Australia", "Bangladesh", "Bhutan", "Brunei", "Myanmar", "Cambodia",
-    "China", "Fiji", "India", "Indonesia", "Japan", "Kiribati",
-    "Laos", "Malaysia", "Maldives", "Marshall Islands", "Micronesia", "Nepal",
-    "New Zealand", "North Korea", "Palau", "Papua New Guinea", "Philippines",
-    "Samoa", "Singapore", "Solomon Islands", "South Korea", "Sri Lanka", "Taiwan",
-    "Thailand", "Timor-Leste", "Tonga", "Tuvalu", "Vanuatu",
-    "Viet Nam", "Russia", "Viet Nam"
+    "Australia",
+    "Bangladesh",
+    "Bhutan",
+    "Brunei",
+    "Myanmar",
+    "Cambodia",
+    "China",
+    "Fiji",
+    "India",
+    "Indonesia",
+    "Japan",
+    "Kiribati",
+    "Laos",
+    "Malaysia",
+    "Maldives",
+    "Marshall Islands",
+    "Micronesia",
+    "Nepal",
+    "New Zealand",
+    "North Korea",
+    "Palau",
+    "Papua New Guinea",
+    "Philippines",
+    "Samoa",
+    "Singapore",
+    "Solomon Islands",
+    "South Korea",
+    "Sri Lanka",
+    "Taiwan",
+    "Thailand",
+    "Timor-Leste",
+    "Tonga",
+    "Tuvalu",
+    "Vanuatu",
+    "Viet Nam",
+    "Russia",
+    "Viet Nam",
 ]
-
-# Header
-st.set_page_config(layout="wide")
-st.write("<h1>SIPRI Recent Data Set 1992-2023</h1>", unsafe_allow_html=True)
-year_range = st.slider("Select year range for order placement dates", 1992, 2023, (1992, 2023))
 
 # Load
 original_df = pd.read_csv("data/sipri-full-rows.csv")
 original_df.reset_index(inplace=True)
 original_df.columns = original_df.columns.map(lambda x: x.strip())
 
-# Source Data
-with st.expander("Source data"):
-    st.dataframe(original_df)
-
-# Columns
-with st.expander("Columns"):
-    column_names = original_df.columns.tolist()
-    filtered_columns = [col for col in column_names if not (col == '' or col.startswith('.') or col.isdigit())]
-    st.write(filtered_columns)
-
-# Clean
-df = original_df[(original_df["Year of order"] >= year_range[0]) & (original_df["Year of order"] <= year_range[1])]
-df = df.dropna(subset=['Recipient'])
-df = df[filtered_columns]
-
-# Filter valid APAC countries
+# Validate valid APAC countries
 unique_recipients = original_df["Recipient"].unique()
 for country in APAC_COUNTRIES:
     if not country in unique_recipients:
         print(country)
         raise AssertionError()
 
+# Process columns
+column_names = original_df.columns.tolist()
+filtered_columns = [
+    col
+    for col in column_names
+    if not (col == "" or col.startswith(".") or col.isdigit())
+]
+
+# Set up, clean, filter pre-sidebar
+apac_rec_df = original_df.dropna(subset=["Recipient"])
+apac_rec_df = apac_rec_df[apac_rec_df["Recipient"].isin(APAC_COUNTRIES)]
+apac_rec_df = apac_rec_df[filtered_columns]
+
+# Header
+st.set_page_config(layout="wide")
+st.write("<h1>SIPRI Recent Data Set 1992-2023</h1>", unsafe_allow_html=True)
+
+# Source dataframe
+with st.expander("Source data"):
+    st.dataframe(original_df)
+
+# Columns
+with st.expander("Available columns"):
+    st.write(filtered_columns)
+
+# APAC countries
 with st.expander("Selected APAC countries"):
+    world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+    apac_map = world[world["name"].isin(APAC_COUNTRIES + ["Vietnam"])]
+    fig = px.choropleth(
+        apac_map,
+        geojson=world.__geo_interface__,
+        locations=apac_map.index,
+        color="name",
+        hover_name="name",
+        title="Selected APAC Countries",
+    )
+    fig.update_geos(projection_type="equirectangular")
+    st.plotly_chart(fig)
     st.write(APAC_COUNTRIES)
 
-with st.expander("Filtered data sets to APAC region"):
-    apac_df = df[(df["Recipient"].isin(APAC_COUNTRIES)) | (df["Supplier"].isin(APAC_COUNTRIES))]
-    st.write("Filtered data to suppliers OR recipients in APAC region:")
-    st.write(apac_df)
-
-    apac_rec_df = df[df["Recipient"].isin(APAC_COUNTRIES)]
-    st.write("Filtered data to recipients in APAC region:")
+# Dataframe of APAC recipients
+with st.expander("Source data filtered on APAC countries"):
     st.write(apac_rec_df)
 
+# Sidebar
+with st.sidebar:
+    st.header("Filters")
+    global_year_range = st.slider("Order Placement Year", 1992, 2023, (1992, 2023))
+    global_recipient_filter = st.multiselect(
+        "Filter by Recipient Countries", apac_rec_df["Recipient"].unique()
+    )
+    global_supplier_filter = st.multiselect(
+        "Filter by Supplier Countries", apac_rec_df["Supplier"].unique()
+    )
+    global_description_filter = st.multiselect(
+        "Filter by Weapon Categories", apac_rec_df["Weapon description"].unique()
+    )
+
+# Create filtered dataframes
+year_filter_df = apac_rec_df[
+    (apac_rec_df["Year of order"] >= global_year_range[0])
+    & (apac_rec_df["Year of order"] <= global_year_range[1])
+]
+each_filter_df = year_filter_df
+year_text = f"{global_year_range[0]}-{global_year_range[1]}"
+rcp_base = "Recipients: "
+rcp_text = "APAC"
+sup_base = "Suppliers: "
+sup_text = "APAC"
+cat_base = "Weapon categories: "
+cat_text = "All"
+if global_recipient_filter:
+    each_filter_df = each_filter_df[
+        each_filter_df["Recipient"].isin(global_recipient_filter)
+    ]
+    rcp_text = ", ".join(global_recipient_filter)
+if global_supplier_filter:
+    each_filter_df = each_filter_df[
+        each_filter_df["Supplier"].isin(global_supplier_filter)
+    ]
+    sup_text = ", ".join(global_supplier_filter)
+if global_description_filter:
+    each_filter_df = each_filter_df[
+        each_filter_df["Weapon description"].isin(global_description_filter)
+    ]
+    cat_text = ", ".join(global_description_filter)
+rcp_full = rcp_base + rcp_text
+sup_full = sup_base + sup_text
+cat_full = cat_base + cat_text
+full_subtitle_text = f"<br><sup>{year_text}; {rcp_full}; {sup_full}; {cat_full}</sup>"
+
+
+# General overview
+st.write(
+    f"<h3>General Overview: {global_year_range[0]}-{global_year_range[1]} </h3>",
+    unsafe_allow_html=True,
+)
 col1, col2 = st.columns(2)
 
 with col1:
-    # Top 8 SIPRI TIV delivered to APAC by country
-    supplier_totals = apac_rec_df.groupby('Supplier')['SIPRI TIV of delivered weapons'].sum().sort_values(ascending=False)
-    top_10_suppliers = supplier_totals.head(8)
-    other_suppliers = supplier_totals[8:].sum()
-    supplier_totals_combined = pd.concat([top_10_suppliers, pd.Series(other_suppliers, index=['Other'])])
-
-    fig, ax = plt.subplots()
-    ax.pie(supplier_totals_combined, labels=supplier_totals_combined.index, autopct='%1.0f%%', startangle=90, counterclock=False)
-    ax.set_title('Top 8 Suppliers to APAC Countries by Total Weapons Value')
-    st.pyplot(fig)
-
-with col2:
-    # Top 12 SIPRI TIV delivered to APAC by country
-    recipient_totals = apac_rec_df.groupby('Recipient')['SIPRI TIV of delivered weapons'].sum().sort_values(ascending=False)
-    top_10_recipients = recipient_totals.head(12)
-    other_recipients = recipient_totals[12:].sum()
-    recipient_totals_combined = pd.concat([top_10_recipients, pd.Series(other_recipients, index=['Other'])])
-
-    fig, ax = plt.subplots()
-    ax.pie(recipient_totals_combined, labels=recipient_totals_combined.index, autopct='%1.0f%%', startangle=90, counterclock=False)
-    ax.set_title('Top 12 Recipients among APAC Countries by Total Weapons Value')
-    st.pyplot(fig)
-
-col1, col2, col3 = st.columns([3, 4, 3])
+    # Top 10 SIPRI TIV delivered to APAC by country
+    supplier_totals = (
+        year_filter_df.groupby("Supplier")["SIPRI TIV of delivered weapons"]
+        .sum()
+        .sort_values(ascending=False)
+    )
+    top_10_suppliers = supplier_totals.head(10)
+    other_suppliers = supplier_totals[10:].sum()
+    supplier_totals_combined = pd.concat(
+        [top_10_suppliers, pd.Series(other_suppliers, index=["Other"])]
+    )
+    fig = px.pie(
+        supplier_totals_combined,
+        values=supplier_totals_combined,
+        names=supplier_totals_combined.index,
+        title="Top 10 Suppliers to APAC Countries by Total Weapons Value",
+    )
+    st.plotly_chart(fig)
 
 with col2:
-    yearly_totals = apac_rec_df.groupby(apac_rec_df['Year of order'])['SIPRI TIV of delivered weapons'].sum()
-    fig, ax = plt.subplots()
-    yearly_totals.plot(kind='line', ax=ax)
-    ax.set_title(f'Total APAC Weapons Value by Year {year_range[0]}-{year_range[1]}')
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Total Weapons Value (millions TIV)')
-    st.pyplot(fig)
+    # Top 10 SIPRI TIV delivered to APAC by country
+    recipient_totals = (
+        year_filter_df.groupby("Recipient")["SIPRI TIV of delivered weapons"]
+        .sum()
+        .sort_values(ascending=False)
+    )
+    top_10_recipients = recipient_totals.head(10)
+    other_recipients = recipient_totals[10:].sum()
+    recipient_totals_combined = pd.concat(
+        [top_10_recipients, pd.Series(other_recipients, index=["Other"])]
+    )
+    fig = px.pie(
+        recipient_totals_combined,
+        values=recipient_totals_combined,
+        names=recipient_totals_combined.index,
+        title="Top 10 Recipients among APAC Countries by Total Weapons Value",
+    )
+    st.plotly_chart(fig)
+
+# Filtered Widgets
+st.write(
+    f"<h3>Filtered Widgets: {global_year_range[0]}-{global_year_range[1]} </h3>",
+    unsafe_allow_html=True,
+)
+
+yearly_totals = each_filter_df.groupby(year_filter_df["Year of order"])[
+    "SIPRI TIV of delivered weapons"
+].sum()
+fig = px.bar(
+    yearly_totals,
+    title=f"Total Weapons Ordered by Year from {global_year_range[0]}-{global_year_range[1]}{full_subtitle_text}",
+)
+fig.update_layout(xaxis_title="Year", yaxis_title="Total Weapons Value (millions TIV)")
+st.plotly_chart(fig)
 
 col1, col2 = st.columns(2)
 
 with col1:
-    recipient_country = st.selectbox("Select a Recipient Country", apac_rec_df['Recipient'].unique())
-    df_filter_rec = apac_rec_df[apac_rec_df['Recipient'] == recipient_country]
-
     # Breakdown of total value by supplier country for the selected recipient country
-    rec_supplier_totals = df_filter_rec.groupby('Supplier')['SIPRI TIV of delivered weapons'].sum().sort_values(ascending=False)
+    rec_supplier_totals = (
+        each_filter_df.groupby("Supplier")["SIPRI TIV of delivered weapons"]
+        .sum()
+        .sort_values(ascending=False)
+    )
     top_rec_suppliers = rec_supplier_totals.head(5)
     other_rec_suppliers = rec_supplier_totals[5:].sum()
-    rec_supplier_totals_combined = pd.concat([top_rec_suppliers, pd.Series(other_rec_suppliers, index=['Other'])])
+    rec_supplier_totals_combined = pd.concat(
+        [top_rec_suppliers, pd.Series(other_rec_suppliers, index=["Other"])]
+    )
 
-    fig, ax = plt.subplots()
-    ax.pie(rec_supplier_totals_combined, labels=rec_supplier_totals_combined.index, autopct='%1.0f%%', startangle=90, counterclock=False)
-    ax.set_title(f'Weapons Value Breakdown by Supplier for {recipient_country}')
-    st.pyplot(fig)
+    fig = px.pie(
+        rec_supplier_totals_combined,
+        values=rec_supplier_totals_combined,
+        names=rec_supplier_totals_combined.index,
+        title=f"Weapons Value Breakdown by Supplier",
+    )
+    st.plotly_chart(fig)
 
 with col2:
-    supplier_country = st.selectbox("Select a Supplier Country", apac_rec_df["Supplier"].unique())
-    df_filter_sup = apac_rec_df[apac_rec_df['Supplier'] == supplier_country]
-
     # Breakdown of total value by recipient country for the selected supplier country
-    sup_recipient_totals = df_filter_sup.groupby('Recipient')['SIPRI TIV of delivered weapons'].sum().sort_values(ascending=False)
+    sup_recipient_totals = (
+        each_filter_df.groupby("Recipient")["SIPRI TIV of delivered weapons"]
+        .sum()
+        .sort_values(ascending=False)
+    )
     top_sup_recipients = sup_recipient_totals.head(5)
     other_sup_recipients = sup_recipient_totals[5:].sum()
-    sup_recipient_totals_combined = pd.concat([top_sup_recipients, pd.Series(other_sup_recipients, index=['Other'])])
+    sup_recipient_totals_combined = pd.concat(
+        [top_sup_recipients, pd.Series(other_sup_recipients, index=["Other"])]
+    )
 
-    fig, ax = plt.subplots()
-    ax.pie(sup_recipient_totals_combined, labels=sup_recipient_totals_combined.index, autopct='%1.0f%%', startangle=90, counterclock=False)
-    ax.set_title(f'Weapons Value Breakdown by Recipient for {supplier_country}')
-    st.pyplot(fig)
+    fig = px.pie(
+        sup_recipient_totals_combined,
+        values=sup_recipient_totals_combined,
+        names=sup_recipient_totals_combined.index,
+        title=f"Weapons Value Breakdown by Recipient",
+    )
+    st.plotly_chart(fig)
 
-# Network graph
-col1, col2, col3 = st.columns([2, 6, 2])
-with col2:
-    st.write("Directed Graph of Total Weapons Value Ordered")
-    select_all = st.checkbox("Show All Weapon Descriptions")
-    if select_all:
-        weapon_description_filter = apac_rec_df["Weapon description"].unique().tolist()
-    else:
-        weapon_description_filter = st.multiselect("Select Weapon Descriptions to include in the network graph", apac_rec_df["Weapon description"].unique())
-    weapon_apac_rec_df = apac_rec_df[apac_rec_df["Weapon description"].isin(weapon_description_filter)]
 
-G = nx.DiGraph()
-for index, row in weapon_apac_rec_df.iterrows():
-    if not G.has_edge(row['Supplier'], row['Recipient']):
-        G.add_edge(row['Supplier'], row['Recipient'], weight=row['SIPRI TIV of delivered weapons'])
-    else:
-        G[row['Supplier']][row['Recipient']]['weight'] += row['SIPRI TIV of delivered weapons']
+@st.cache_data
+def build_graph(each_filter_df):
+    G = nx.DiGraph()
+    for index, row in each_filter_df.iterrows():
+        if not G.has_edge(row["Supplier"], row["Recipient"]):
+            G.add_edge(
+                row["Supplier"],
+                row["Recipient"],
+                weight=row["SIPRI TIV of delivered weapons"],
+            )
+        else:
+            G[row["Supplier"]][row["Recipient"]]["weight"] += row[
+                "SIPRI TIV of delivered weapons"
+            ]
+    net = Network(height="700px", width="100%", directed=True)
+    net.from_nx(G)
+    net.repulsion()
+    for edge in net.edges:
+        edge["title"] = f"log(TIV): {round(math.log(edge['width']+1, 6), 2)}"
+        edge["value"] = math.log(edge["width"] + 1, 6)
+    return net
 
-net = Network(height='700px', width='100%', directed=True)
-net.from_nx(G)
-net.repulsion()
-import math
-for edge in net.edges:
-    edge['title'] = f"log(TIV): {round(math.log(edge["width"]+1, 10), 2)}"
-    edge['value'] = math.log(edge["width"]+1, 10)
 
-net.save_graph('graph.html')
-with open('graph.html', 'r', encoding='utf-8') as f:
-    graph_html = f.read()
+net = build_graph(each_filter_df)
+with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as tmp_file:
+    net.save_graph(tmp_file.name)
+    tmp_file.seek(0)
+    graph_html = tmp_file.read().decode("utf-8")
 
-with col2:
-    components.html(graph_html, height=750)
+st.write(
+    "<h4>Directed Graph of Total Weapons Value Ordered</h4>", unsafe_allow_html=True
+)
+components.html(graph_html, height=750)
+
+# Globally Filtered Dataframe
+with st.expander("Source data filtered on global configuration options"):
+    st.write(each_filter_df)
